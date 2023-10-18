a11ef94a1631e931e73be6b2e63d5d7ad0051278
CesareDev <corsicesare.lavoro@gmail.com>
2023-10-18 16:51:50 +0200

    Fixed teleport position in Entrance when win

--- D&A/src/Levels/Other/Entrance.cpp
+++ D&A/src/Levels/Other/Entrance.cpp
@@ -16,9 +16,10 @@ void Entrance::Init(const ResourceManager& resourceManager, sf::DynamicQuadTree<
 	m_Transition.Init(resourceManager);
 	m_Label.Init(resourceManager, CONFIG::LEVELS_INFO[GetId()].name);
 
-	if (SAVE::DEMON_BOSS_DEFEATED && SAVE::ORC_BOSS_DEFEATED && SAVE::UNDEAD_BOSS_DEFEATED)
+	bool end = SAVE::DEMON_BOSS_DEFEATED && SAVE::ORC_BOSS_DEFEATED && SAVE::UNDEAD_BOSS_DEFEATED;
+
+	if (end)
 	{
-		m_Player->SetPosition({ m_Player->getPosition().x, m_Player->getPosition().y + 160.f });
 		m_Map.load("../res/map/entranceWin.tmx", &resourceManager.GetTilesetTexture());
 		m_Princess.Init(resourceManager, { 232.f, 80.f });
 		m_Princess.SetPlayer(player);
@@ -32,40 +33,73 @@ void Entrance::Init(const ResourceManager& resourceManager, sf::DynamicQuadTree<
 	//this func-^
 	//clear too
 
-	if (entranceIndex == 0)
-		m_Player->SetPosition({ 224.f, 432.f }); //starting point
-	else if (entranceIndex == 1)
-		m_Player->SetPosition({ 48.f, 240.f }); //orcs1
-	else if (entranceIndex == 2)
-		m_Player->SetPosition({ 176.f, 48.f }); //demons2
-	else if (entranceIndex == 3)
-		m_Player->SetPosition({ 288.f, 48.f }); //demons1
-	else if (entranceIndex == 4)
-		m_Player->SetPosition({ 416.f, 240.f }); //undeads1
+	if (end)
+	{
+		if (entranceIndex == 0)
+			m_Player->SetPosition({ 224.f, 592.f }); //starting point
+		else if (entranceIndex == 1)
+			m_Player->SetPosition({ 48.f, 400.f }); //orcs1
+		else if (entranceIndex == 2)
+			m_Player->SetPosition({ 176.f, 208.f }); //demons2
+		else if (entranceIndex == 3)
+			m_Player->SetPosition({ 288.f, 208.f }); //demons1
+		else if (entranceIndex == 4)
+			m_Player->SetPosition({ 416.f, 400.f }); //undeads1
+	}
+	else
+	{
+		if (entranceIndex == 0)
+			m_Player->SetPosition({ 224.f, 432.f }); //starting point
+		else if (entranceIndex == 1)
+			m_Player->SetPosition({ 48.f, 240.f }); //orcs1
+		else if (entranceIndex == 2)
+			m_Player->SetPosition({ 176.f, 48.f }); //demons2
+		else if (entranceIndex == 3)
+			m_Player->SetPosition({ 288.f, 48.f }); //demons1
+		else if (entranceIndex == 4)
+			m_Player->SetPosition({ 416.f, 240.f }); //undeads1
+	}
 
 	m_AStar.init(m_Map);
 	m_Hud.Init(resourceManager, player, m_Map);
 	m_Camera.Init(m_Player->GetCenter(), { 0.f, 0.f, (float)m_Map.getMapSize().x, (float)m_Map.getMapSize().y }, { 0.f, 0.f, GLOBAL::WIN_WIDTH / 5.f, GLOBAL::WIN_HEIGHT / 5.f });
 
-	m_Ladder1.Init(resourceManager, { 32.f, 240.f });
+	if (end)
+	{
+		m_Ladder1.Init(resourceManager, { 32.f, 400.f });
+		m_Ladder2.Init(resourceManager, { 160.f, 208.f });
+		m_Ladder3.Init(resourceManager, { 304.f, 208.f });
+		m_Ladder4.Init(resourceManager, { 432.f, 400.f });
+	}
+	else
+	{
+		m_Ladder1.Init(resourceManager, { 32.f, 240.f });
+		m_Ladder2.Init(resourceManager, { 160.f, 48.f });
+		m_Ladder3.Init(resourceManager, { 304.f, 48.f });
+		m_Ladder4.Init(resourceManager, { 432.f, 240.f });
+	}
+
 	m_Ladder1.SetTeleportLevel(LevelID::OrcsOne, 0);
-
-	m_Ladder2.Init(resourceManager, { 160.f, 48.f });
 	m_Ladder2.SetTeleportLevel(LevelID::DemonsTwo, 0);
-
-	m_Ladder3.Init(resourceManager, { 304.f, 48.f });
 	m_Ladder3.SetTeleportLevel(LevelID::DemonsOne, 0);
-
-	m_Ladder4.Init(resourceManager, { 432.f, 240.f });
 	m_Ladder4.SetTeleportLevel(LevelID::UndeadsOne, 0);
 
-	m_StoryMessage.Init(resourceManager, { 224.f, 384.f });
+	if (end)
+	{
+		m_StoryMessage.Init(resourceManager, { 224.f, 544.f });
+		m_CommandMessage.Init(resourceManager, { 224.f, 463.f });
+	}
+	else
+	{
+		m_StoryMessage.Init(resourceManager, { 224.f, 384.f });
+		m_CommandMessage.Init(resourceManager, { 224.f, 304.f });
+	}
+
 	if (GLOBAL::JOYSTICK_AVAILABLE)
 		m_StoryMessage.SetText("Orcs demons and undeads\nhave invaded the castle.\nDefeat them and save the princess\n-- Press Space to close -- / -- X --");
 	else
 		m_StoryMessage.SetText("Orcs demons and undeads\nhave invaded the castle.\nDefeat them and save the princess\n-- Press Space to close --");
 
-	m_CommandMessage.Init(resourceManager, { 224.f, 304.f });
 	if (GLOBAL::JOYSTICK_AVAILABLE)
 		m_CommandMessage.SetText("Command / PS4 joystick:\nWASD to move / Left Analog\nLeft Mouse to attack / R1\nScrool to change weapon / L2 - R2\nE for the shop - Square");
 	else
